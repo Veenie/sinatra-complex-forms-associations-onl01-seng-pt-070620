c8470d95e9b74fa0fcc3cac981d9194d87c9abdf
Raveen K Sharma <raveen08@gmail.com>
2020-10-13 18:59:27 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -43,7 +43,7 @@ class PetsController < ApplicationController
     @pet.update(params[:pet])
     
     if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     
     @pet.save
